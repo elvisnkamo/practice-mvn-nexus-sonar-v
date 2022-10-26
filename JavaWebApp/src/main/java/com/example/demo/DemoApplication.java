9b5e3519a9731940c077a79eee37ec3f876766c4
PANDAH INC <pandah@PANDAHs-MBP.ht.home>
2022-10-25 21:40:21 -0400

    fix again

--- JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
+++ JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
@@ -6,9 +6,9 @@ public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
 
+        String name = "vamsi";
 
-        String name = "elvis";
-
+        
         System.out.println(name);
     }
 }
